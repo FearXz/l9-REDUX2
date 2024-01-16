0a2ba45d97f1fc8aa010e6cd5ca5a10218bd6d87
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-16 21:39:54 +0100

    ok

--- src/redux/action/action.js
+++ src/redux/action/action.js
@@ -12,3 +12,22 @@ export const fetchJobsAction = (url, query) => async (dispatch) => {
     }
   } catch (error) {}
 };
+
+/* export const fetchJobs = (url, query) => async (dispatch) => {
+  try {
+    const response = await fetch(url + query + "&limit=20");
+
+    if (response.ok) {
+      const { data } = await response.json();
+      dispatch({
+        type: "FETCH_JOBS_SUCCESS",
+        payload: data,
+      }); // Azione per indicare il successo del fetch
+    } else {
+      throw new Error("Errore nel recupero dei risultati");
+    }
+  } catch (error) {
+    // Puoi gestire gli errori qui, se necessario
+    console.error("Errore nel fetch:", error.message);
+  }
+}; */
--- src/redux/reducers/job.js
+++ src/redux/reducers/job.js
@@ -17,3 +17,24 @@ const JobReducer = (state = initialState, action) => {
 };
 
 export default JobReducer;
+
+/* import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  jobs: [],
+  status: "idle",
+};
+
+const fetchJobSlice = createSlice({
+  name: "fetchJob",
+  initialState,
+  reducers: {
+    FETCH_JOBS_SUCCESS: (state, action) => {
+      state.status = "succeeded";
+      state.jobs = action.payload;
+    },
+  },
+});
+
+export const { FETCH_JOBS_SUCCESS } = fetchJobSlice.actions;
+export default fetchJobSlice.reducer; */
